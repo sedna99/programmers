e649ab60f76a3050c40d9700e720e64ebbc283a7
sedna99 <105535152+sedna99@users.noreply.github.com>
2024-08-08 18:32:34 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 3.98 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -6,8 +6,8 @@ class Solution {
     public String solution(int[] numbers, String hand) {
         int[] parsed = Arrays.stream(numbers).map(i -> i == 0 ? 11 : i).toArray();
         char defaultHand = hand.toUpperCase().charAt(0);
-        int[] lCursor = new int[] {3,0};
         int[] rCursor = new int[] {3,2};
+        int[] lCursor = new int[] {3,0};
         char[] result = new char[parsed.length];
         for(int i = 0; i < parsed.length; i++) {
             int number = parsed[i];
@@ -33,7 +33,6 @@ class Solution {
     public char judge(int[] rCursor, int[] lCursor, int[] coord, char defaultHand) {
         int rDist = Math.abs(rCursor[0] - coord[0]) + Math.abs(rCursor[1] - coord[1]);
         int lDist = Math.abs(lCursor[0] - coord[0]) + Math.abs(lCursor[1] - coord[1]);
-        System.out.println("rDist: " + rDist + " lDist: " + lDist);
         if(rDist > lDist) return 'L';
         else if(rDist < lDist) return 'R';
         else return defaultHand;
