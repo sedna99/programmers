0e4a7970ed4fa40aa08b2ca75b6195c03d312020
sedna99 <105535152+sedna99@users.noreply.github.com>
2024-08-08 16:18:31 +0900

    [level 1] Title: 신규 아이디 추천, Time: 3.68 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String solution(String new_id) {
+        return step7(step6(step5(step4(step3(step2(step1(new_id)))))));
+    }
+
+    public String step1(String before) {
+        return before.toLowerCase();
+    }
+
+    public String step2(String before) {
+        return before.replaceAll("[^0-9a-z\\-_.]", "");
+    }
+
+    public String step3(String before) {
+        return before.replaceAll("\\.+", ".");
+    }
+
+    public String step4(String before) {
+        return before.replaceAll("^\\.", "").replaceAll("\\.$", "");
+    }
+
+    public String step5(String before) {
+        return before.isEmpty() ? "a" : before;
+    }
+
+    public String step6(String before) {
+        return before.length() > 15 ? before.substring(0, 15).replaceAll("\\.$", "") : before;
+    }
+
+    public String step7(String before) {
+        StringBuilder after = new StringBuilder(before);
+        while(after.length() < 3) {
+            after.append(after.charAt(after.length() - 1));
+        }
+        return after.toString();
+    }
+}
