40bb508c47ce8667553ae9595a224c50c9353e15
sedna99 <105535152+sedna99@users.noreply.github.com>
2024-08-09 12:03:57 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 9.42 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -11,14 +11,7 @@ class Solution {
         keyMap.put("remain", 3);
         List<List<Integer>> resultList = new ArrayList<>();
         int sortIndex = keyMap.get(sort_by);
-        for (int i = 0; i < data.length; i++) {
-            int[] row = data[i];
-            int index = keyMap.get(ext);
-            if(row[index] < val_ext) {
-                resultList.add(IntStream.of(row).boxed().collect(Collectors.toList()));
-            };
-        }
-        return resultList.stream().sorted((a, b) -> a.get(sortIndex) - b.get(sortIndex)).map(l -> l.stream().mapToInt(Integer::intValue).toArray())
-                .toArray(int[][]::new);
+        int compareIndex = keyMap.get(ext);
+        return Arrays.stream(data).filter(row -> row[compareIndex] < val_ext).sorted(Comparator.comparingInt(a -> a[sortIndex])).toArray(int[][]::new);
     }
 }
