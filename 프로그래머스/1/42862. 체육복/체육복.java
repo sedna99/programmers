758d8eb8d9763d7bde913b77d6fa10d1e4b5ffb8
sedna99 <105535152+sedna99@users.noreply.github.com>
2024-08-09 15:05:16 +0900

    [level 1] Title: 체육복, Time: 1.81 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -3,33 +3,19 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] map = new int[n];
+        int[] map = new int[n+2];
         Arrays.setAll(map, i->1);
-        
+
         for(int lostIdx : lost) {
-            map[lostIdx - 1]--;
+            map[lostIdx]--;
         }
 
         for(int reserveIdx : reserve) {
-            map[reserveIdx - 1]++;
+            map[reserveIdx]++;
         }
 
-        for(int i = 0; i < n; i++) {
-            if(i == 0) {
-                if(map[i+1] == 2) {
-                    map[i+1] = 1;
-                    map[i] = 1;
-                }
-            }
-            else if(i == n-1) {
-                if(map[i] == 0) {
-                    if (map[i-1] == 2) {
-                        map[i-1] = 1;
-                        map[i] = 1;
-                    }
-                }
-            }
-            else if(map[i] == 0) {
+        for(int i = 1; i < n+1; i++) {
+            if(map[i] == 0) {
                 if(map[i-1] == 2) {
                     map[i-1] = 1;
                     map[i] = 1;
@@ -39,6 +25,7 @@ class Solution {
                 }
             }
         }
-        return Long.valueOf(IntStream.of(map).filter(i -> i > 0).count()).intValue();
+
+        return Long.valueOf(IntStream.of(map).filter(i -> i > 0).count()).intValue() - 2;
     }
 }
